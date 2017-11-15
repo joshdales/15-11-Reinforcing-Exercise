b9a0c9209bda45eb0e46699d5a1536e1a8540f70
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-15 12:22:49 -0500

    Createn test for to_hash that runs correctly

--- test_team.rb
+++ test_team.rb
@@ -4,4 +4,15 @@ require './team.rb'
 
 class TestTeam < MiniTest::Test
 
+  def setup
+    @team = Team.new('tree', 13, 'Jill, Jack, Jane, Joe')
+  end
+
+  def test_to_hash
+    result = @team.to_hash
+    expected = {team_name: 'tree', level: 13, points: 0}
+    assert_equal expected, result
+  end
+
+
 end
